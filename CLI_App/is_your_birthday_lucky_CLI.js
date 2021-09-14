6b042735a5749bcb9797fe8d9d40e39b57aae1de
Suraj Rai <77954411+devsurajrai@users.noreply.github.com>
2021-09-14 20:31:11 +0530

    refactor: code cleanup

--- CLI_App/is_your_birthday_lucky_CLI.js
+++ CLI_App/is_your_birthday_lucky_CLI.js
@@ -1,24 +1,22 @@
-var readlineSync=require('readline-sync');
+var readlineSync = require('readline-sync');
 
-var birthDay=readlineSync.question("Please enter your DOB (Ex: 11121997 ): ");
+var birthDay = readlineSync.question("Please enter your DOB (Ex: 11121997 ): ");
 console.log();
 
-var userLuckyNumber=readlineSync.question("Please enter your lucky number: ");
+var userLuckyNumber = readlineSync.question("Please enter your lucky number: ");
 
-var birthdaySum=0;
-var lastDigit=0;
+var lastDigit = 0;
+var birthdaySum = 0;
 
-while(birthDay){
-    lastDigit=birthDay%10;
-    birthdaySum+=lastDigit;
-    birthDay=Math.floor(birthDay/10);
+while (birthDay) {
+    lastDigit = birthDay % 10;
+    birthdaySum += lastDigit;
+    birthDay = Math.floor(birthDay / 10);
 }
 console.log()
 
-if(birthdaySum%userLuckyNumber==0){
+if (birthdaySum % userLuckyNumber == 0) {
     console.log("Congratulations ! Your birthday is lucky. ")
-}
-
-else{
+} else {
     console.log("Sorry ! Your birthday is not lucky")
 }
--- app.js
+++ app.js
@@ -1,42 +1,39 @@
 var birthDay = document.querySelector('#dateOfBirth');
 var userLuckyNumber = document.querySelector('#userLuckyNumber');
 var button = document.querySelector('#check')
+var error = document.querySelector('.error')
 var closeButton = document.querySelectorAll('.modal_close_btn')
-var error=document.querySelector('.error')
 
 button.addEventListener('click', clickHandler = () => {
-    
-        birthDayArr=[...birthDay.value]
-        console.log(birthDayArr)
-        birthDayValue=""
-        for(let items in birthDayArr){
-            if(birthDayArr[items]!=="-"){
-                birthDayValue+=birthDayArr[items]
-            }
+
+    birthDayArr = [...birthDay.value]
+    console.log(birthDayArr)
+    birthDayValue = ""
+    for (let items in birthDayArr) {
+        if (birthDayArr[items] !== "-") {
+            birthDayValue += birthDayArr[items]
         }
-        if(birthDayValue){
+    }
+    if (birthDayValue) {
         var birthdaySum = 0;
         var lastDigit = 0;
-        birthDayValue=parseInt(birthDayValue)
+        birthDayValue = parseInt(birthDayValue)
         var luckyNumberValue = userLuckyNumber.value;
         while (birthDayValue) {
             lastDigit = birthDayValue % 10;
             birthdaySum += lastDigit;
             birthDayValue = Math.floor(birthDayValue / 10);
         }
-    
+
         if (birthdaySum % luckyNumberValue == 0) {
             document.querySelector('#modal_container_true').style.display = 'block'
-        } 
-        
-        else {
+        } else {
             document.querySelector('#modal_container_false').style.display = 'block'
-            
+
         }
-    }
-    else{
-         error.innerHTML="Please enter a valid input"
-         error.style.color="red"
+    } else {
+        error.innerHTML = "Please enter a valid input"
+        error.style.color = "red"
     }
 
 
